6815e413a79d6d01cb87b3ad2a39deb5321653cf
rarexxj <rarexxj@163.com>
2017-04-14 18:04:56 +0800

    xxj

--- Html/mjs/buy.js
+++ Html/mjs/buy.js
@@ -9,13 +9,31 @@ $(function () {
                 goodsCategory: 1,
                 pageNo: 1,
                 limit: 10
+            },
+            regdata:{
+                PhoneNumber:'',
+                Password:'123456',
+                SmsVerifyCode:'',
+                ChannelId:'',
+                InvitationCode:'',
+                OpenId:'',
+                StoreId:'',
+                StoreName:''
+            },
+            shopinfo:[],
+            duanxdata:{
+                PhoneNumber:'',
+                RequestType:'0'
             }
 
         },
         ready: function () {
             var _this = this;
             // _this.proinfoajax();
+            _this.yanzcode();
             _this.layclose();
+            _this.register();
+            _this.shopaddress();
             _this.$nextTick(function () {
             })
         },
@@ -43,13 +61,88 @@ $(function () {
                     $(this).hide();
                 })
                 $('#main').on('click','.layer-xx',function () {
-                    $(this).hide();
+                    // $(this).hide();
                 })
                 $('#main').on('click','.lay-close',function () {
                     $('.layer').show();
                 })
                 $('#main').on('click','.per-xx',function () {
                     $('.layer-xx').show();
+                })
+            },
+            register:function () {
+                var _this=this;
+                var reg = /^1[3|4|5|7|8]\d{9}$/
+                $('#main').on('click','.submit',function () {
+                    _this.regdata.PhoneNumber=$('#tel').val();
+                    _this.regdata.NickName=$('#name').val();
+                    _this.regdata.SmsVerifyCode=$('#yanzm').val();
+                    _this.regdata.StoreId=$('#shop-box option:selected').attr('data-id');
+                    _this.regdata.StoreName=$('#shop-box option:selected').html();
+                    if(!reg.test($('#tel').val())){
+                        $.oppo("手机号格式有误", 1);
+                        return false;
+                    }else if($('#tel').val() ==''){
+                        $.oppo("请输入手机号", 1);
+                        return false;
+                    }else if($('#name').val()==''){
+                        $.oppo("请输入姓名", 1);
+                        return false;
+                    }else if($('#yanzm').val()==''){
+                        $.oppo("请输入验证码", 1);
+                        return false;
+                    }else{
+                        console.log(_this.regdata)
+                        $.ajax({
+                            url:'/Api/v1/Member',
+                            type:'POST',
+                            dataType:'json',
+                            data:_this.regdata
+                        }).done(function (rs) {
+                            if(rs.returnCode=='200'){
+
+                            }
+                        })
+                    }
+                })
+            },
+            shopaddress:function () {
+                var _this=this;
+                $.ajax({
+                    url:'/Api/v1/StoreList/Activity',
+                    type:'get',
+                    dataType:'json'
+                }).done(function (rs) {
+                    if(rs.returnCode='200'){
+                        _this.shopinfo=rs.data;
+                    }
+                })
+            },
+            yanzcode:function () {
+                var reg = /^1[3|4|5|7|8]\d{9}$/;
+                var _this=this;
+                $('#main').on('click','.get',function () {
+                    console.log(_this.duanxdata)
+                    _this.duanxdata.PhoneNumber=$('#tel').val();
+                    if($('#tel').val() ==''){
+                        $.oppo("请输入手机号", 1);
+                        return false;
+                    }else if(!reg.test($('#tel').val())){
+                        $.oppo("手机号格式有误", 1);
+                        return false;
+                    }else{
+                        $.ajax({
+                            url:'/Api/v1/Member/SendCode',
+                            type:'POST',
+                            dataType:'json',
+                            data:_this.duanxdata
+                        }).done(function (rs) {
+                            if(rs.returnCode=='200'){
+                                CountDown($('.get'));
+                                $.oppo('短信验证码已发送',1)
+                            }
+                        })
+                    }
                 })
             }
         }
